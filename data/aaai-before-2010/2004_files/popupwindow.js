8a340cc991678788131e1a06e7175e162e6c23a9
czxttkl <czxttkl@gmail.com>
2014-08-11 20:25:24 -0400

    add aiii html data

---
+++ data/aaai-before-2010/2004_files/popupwindow.js
@@ -0,0 +1,8 @@
+<!--//BEGIN Script
+
+function new_window(url) {
+
+link = window.open(url,"Link","toolbar=0,location=0,directories=0,status=0,menubar=1,scrollbars=1,resizable=1,width=300,height=400,left=40,top=80");
+
+}
+//END Script-->
