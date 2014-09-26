08b041410e367a7efd5f5c61714f561529c181f7
czxttkl <czxttkl@gmail.com>
2014-09-26 11:11:53 -0400

    add icids, icvs and tidse

--- src/main/java/io/metadata/test/Test.java
+++ src/main/java/io/metadata/test/Test.java
@@ -1,5 +1,6 @@
 package io.metadata.test;
 
+import io.metadata.misc.Downloader;
 import io.metadata.misc.Utils;
 
 public class Test {
@@ -10,8 +11,6 @@ public class Test {
     public static void main(String[] args) {
 //        String a = "\rWarp Speed: Path Planning for Star Trek reg.: Armada";
 //        a = Utils.removeNewLine(a);
-        long a = 1000000999;
-        System.out.println(String.format("%d%n", a));
 
     }
 
